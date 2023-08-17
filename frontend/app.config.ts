8d6fa56daa6323e581d4b731e0a63fd887c7e572
huayiwen <paranoiawhy@gmail.com>
2023-08-17 18:07:37 +0800

    fix error

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,5 +1,16 @@
+// https://github.com/nuxt-modules/icon/pull/63#issuecomment-1506079746
+export interface NuxtIconModuleOptions {
+  size?: string | false;
+  class?: string;
+  aliases?: { [alias: string]: string };
+}
+
 export default defineAppConfig({
   // error TS2339: Property 'aliases' does not exist on type '{}'.
   // https://github.com/nuxt-modules/icon/pull/63#issuecomment-1480219327
-  nuxtIcon: {},
+  nuxtIcon: {
+    size: "24px", // default <Icon> size applied
+    class: "icon", // default <Icon> class applied
+    aliases: {},
+  } as NuxtIconModuleOptions,
 });
