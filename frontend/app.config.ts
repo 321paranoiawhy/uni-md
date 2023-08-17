161f0b5fa4b112ed60e072dad28e0da550bd5de6
huayiwen <paranoiawhy@gmail.com>
2023-08-17 18:04:47 +0800

    fix error

---
+++ frontend/app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+  // error TS2339: Property 'aliases' does not exist on type '{}'.
+  // https://github.com/nuxt-modules/icon/pull/63#issuecomment-1480219327
+  nuxtIcon: {},
+});
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   pages: true,
-  // error TS2339: Property 'aliases' does not exist on type '{}'.
-  // https://github.com/nuxt-modules/icon/pull/63#issuecomment-1480219327
-  nuxtIcon: {},
   app: {
     // https://nuxt.com/docs/api/configuration/nuxt-config#baseurl
     baseURL: "/uni-md",
